269cdbfbcaf2b8e05b77e8960bb904a710c90e68
Amber Falbo <amberannfalbo@gmail.com>
2020-07-07 15:51:21 -0700

    first half of feature 2 complete

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 'use strict';
+const dropDownItems = [];
 
 // grab page-1.json w/ajax
 $.ajax('data/page-1.json', {method: 'GET', datatype: 'JSON'})
   .then(data => {
     data.forEach(obj => {
       // feed ajax through constructor function
-      new Horns(obj).hornBuilder();
+      let newItem = new Horns(obj);
+      
+      newItem.hornBuilder();
+      newItem.dropDownBuilder();
     });
+    renderDropDown();
   })
 
 // constructor function
@@ -31,5 +36,29 @@ Horns.prototype.hornBuilder = function(){
   $('main').append($newTemplate);
 }
 
+// feature 2
 
+// create <select> element drop down menu which contains unique <options> elements extracted dynamically from the JSON file, one for each keyword.
+
+
+Horns.prototype.dropDownBuilder = function(){
+  if(dropDownItems.includes(this.keyword)===false){
+    dropDownItems.push(this.keyword)
+  }
+}
+
+function renderDropDown(){
+  const dropDown = $(`select`);
+  dropDownItems.forEach(value => {
+    let $dropDownMenu = $(`<option>${value}</option>`);
+    dropDown.append($dropDownMenu);
+  })
+}
+
+
+
+// use and event handler to respond when the user choses an option from the select menu.
+
+
+// hide all of the images, then show those whose keyword matches the option chosen
 
